779c0551a44ad1bd38aebeb828bf44b3a0aaa6ac
j35u54n6u10 <j35u54n6u10@gmail.com>
2025-01-28 00:54:59 -0400

    Last changes

--- src/main.py
+++ src/main.py
@@ -4,18 +4,21 @@ import flet as ft
 def main(page: ft.Page):
 
     def x_count(e):
-        while input_sn.value < input_en.value:
-            sn = int(input_sn.value)
-            sn += 1
-            print(f"X{sn}")
-            #input_sn.value = int(input_sn.value) + 1
-            #show_count.value = "X" + str(input_sn.value)
+        sequence = ""
+        for i in range (int(input_sn.value, int(input_en.value) + 1)):
+            sequence += f"X{str(int(input_sn.value))} \n"
+            text_count.value = sequence
+            page.update()
 
-    def x_count_test (e):
-        while input_sn.value < input_en.value:
-            input_sn.value = input_sn.value + 1
-            print(f"X{input_sn.value}")
-           
+        # while int(input_sn.value) < int(input_en.value):
+        #
+        #     input_sn.value = int(input_sn.value) + 1
+        #     # show_count.value = "X" + str(int(input_sn.value)) + "\n"
+        #     text_count.value = f"X{str(int(input_sn.value))}\n"
+        #
+        #     text_count.update()
+        #
+        
 
     title_x = ft.Text("X Counter", size=25)
     # Start number
@@ -24,20 +27,20 @@ def main(page: ft.Page):
     # End number
     #text_en = ft.Text ("Enter end number:", size=12, weight=ft.FontWieght.W_100)
     input_en = ft.TextField(label="End number")
-
+    text_count = ft.TextField(value="", text_size=15, multiline=True, read_only=True) 
     # Generate botton
     gene_botton = ft.CupertinoFilledButton(content=ft.Text("Generate"), 
                                            opacity_on_click=0.5,
-                                           on_click= x_count_test)
+                                           on_click= x_count)
     # Show count
     show_count = ft.TextField(label="Count", read_only=True)
 
     #copy botton
     copy_botton = ft.CupertinoFilledButton(content=ft.Text("Copy"),
                                            opacity_on_click=0.5,
-                                           on_click= print("Copy"))
+                                           on_click= lambda e: page.set_clipboard(text_count.value))
 
 
-    page.add(title_x, input_sn, input_en, gene_botton, show_count, copy_botton)
+    page.add(title_x, input_sn, input_en, gene_botton, text_count, copy_botton)
 
 ft.app(main)
